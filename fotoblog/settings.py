f7eebde56fe2a6d276769521dfd881703fbcd626
dzikers <dzikers@gmail.com>
2022-02-12 17:49:54 +0700

    customize the user model2

--- fotoblog/settings.py
+++ fotoblog/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'blog',
 ]
 
+AUTH_USER_MODEL = 'authentication.User'
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
