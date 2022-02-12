e5e6ac0bf0eee9b68955a831d3c0b20a2c5ca3a1
dzikers <dzikers@gmail.com>
2022-02-12 17:48:22 +0700

    customize the user model

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+
+    CREATOR = 'CREATOR'
+    SUBSCRIBER = 'SUBSCRIBER'
+
+    ROLE_CHOICE = (
+        (CREATOR, 'Creator'),
+        (SUBSCRIBER, 'Subscriber'),
+    )
+
+    profile_photo = models.ImageField()
+    role = models.CharField(max_length=30, choices=ROLE_CHOICE)
+
+
