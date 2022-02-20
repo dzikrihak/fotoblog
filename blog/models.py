8a2301bff2acd5e3a8657a9b41cd914e7d91a6d7
dzikers <dzikers@gmail.com>
2022-02-20 23:33:27 +0700

    Overriding Model Methods(save method) with resize image

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.db import models
+from PIL import Image
 
 
 class Photo(models.Model):
@@ -7,6 +8,19 @@ class Photo(models.Model):
     caption = models.CharField(max_length=128, blank=True)
     uploader = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     date_created = models.DateTimeField(auto_now_add=True)
+
+    IMAGE_MAX_SIZE = (800, 800)
+
+    def resize_image(self):
+        image = Image.open(self.image)
+        image.thumbnail(self.IMAGE_MAX_SIZE)
+        # save the resized image to the file system
+        # this is not the model save method!
+        image.save(self.image.path)
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        self.resize_image()
 
 
 class Blog(models.Model):
