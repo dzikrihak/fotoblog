5844086f8731a42e19c3cbccb1e0e7b870820100
dzikers <dzikers@gmail.com>
2022-02-23 08:39:05 +0700

    Filter QuerySets Based on Advanced Criteria

--- blog/models.py
+++ blog/models.py
@@ -27,7 +27,7 @@ class Blog(models.Model):
     photo = models.ForeignKey(Photo, null=True, on_delete=models.SET_NULL, blank=True)
     title = models.CharField(max_length=128)
     content = models.CharField(max_length=5000)
-    author = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+    # author = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
     date_created = models.DateTimeField(auto_now_add=True)
     starred = models.BooleanField(default=False)
     word_count = models.IntegerField(null=True)
--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,36 @@
+from itertools import chain
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required, permission_required
 from django.shortcuts import get_object_or_404
 from . import forms
 from . import models
 from django.forms import formset_factory
+from django.db.models import Q
+
+
 
 @login_required
 def home(request):
-    photos = models.Photo.objects.all()
-    blogs = models.Blog.objects.all()
-    return render(request, 'blog/home.html', context={'photos': photos, 'blogs': blogs})
+    blogs = models.Blog.objects.filter(
+        Q(contributors__in=request.user.follows.all()) | Q(starred=True)
+    )
+    photos = models.Photo.objects.filter(
+        uploader__in=request.user.follows.all()).exclude(blog__in=blogs
+    )
+
+    blogs_and_photos = sorted(
+        chain(blogs, photos),
+        key=lambda instance: instance.date_created,
+        reverse=True
+    )
+    # context = {
+    #     'blogs': blogs,
+    #     'photos': photos,
+    # }
+    context = {
+        'blogs_and_photos': blogs_and_photos,
+    }
+    return render(request, 'blog/home.html', context=context)
 
 
 @login_required
@@ -110,3 +131,10 @@ def follow_users(request):
             form.save()
             return redirect('home')
     return render(request, 'blog/follow_users_form.html', context={'form': form})
+
+
+def photo_feed(request):
+    photos = models.Photo.objects.filter(
+        uploader__in=request.user.follows.all()).order_by('-date_created')
+    context = {'photos': photos, }
+    return render(request, 'blog/photo_feed.html', context=context)
